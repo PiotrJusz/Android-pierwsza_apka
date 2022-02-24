3790a1ff93afc890610015d2fe227c95df9c762d
PiotrJusz <94100033+PiotrJusz@users.noreply.github.com>
2022-02-24 06:20:20 +0000

    Dodanie wyświetlania obrazka po wpisaniu tekstu: "Hello World!".

--- src/main/java/com/example/myapplication2_basicapp/DisplayMessageActivity.java
+++ src/main/java/com/example/myapplication2_basicapp/DisplayMessageActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
@@ -12,17 +15,27 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+        ImageView imageView;
+        imageView = (ImageView)findViewById(R.id.imageView);
 
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
-        //String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        if (message.equals("Hello World!")) {
+            Log.i("Message:", message);
+            message = "Witaj Wielki Programisto!";
+            imageView.setVisibility(View.VISIBLE);
+        }
+        else{
+            imageView.setVisibility(View.GONE);
+        }
+
 
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView);
-        //TextView.setText(intent.getStringExtra(MainActivity));
+        //ImageViev imageViev = findViewById(R.id.imageView);
         textView.setText(message);
     }
-
-
 }
+
+
